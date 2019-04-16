4e599725fd1a4747f6c17df5d36f2897ab52ca0a
Saravanan N <saravanan109587@gmail.com>
2019-04-16 13:49:21 +0530

    initialize sdk command added 4

--- src/Tripos.ios.ts
+++ src/Tripos.ios.ts
@@ -1,7 +1,7 @@
 import { Common } from './Tripos.common';
 
  let sharedVtp:any;
-//command added 112124512sdf
+//command added 112124512sdf xc
 
  export function initializeSDK(){
     sharedVtp =triPOSMobileSDK.sharedVtp;
